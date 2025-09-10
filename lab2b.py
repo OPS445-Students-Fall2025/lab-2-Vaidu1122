6b4bf74c0962b226b7a47bd1726f669712ecf2a8
Vaidehi Patel <vpatel278@myseneca.ca>
2025-09-10 19:12:35 -0400

    Added Lab 2 b

---
+++ lab2b.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Name: Vaidehi Patel
+# OPS 445 NDD
+# Std ID: 166249219
+
+# Lab 2 Script 2 - Lab 2 b
+
+name = input('Name: ')
+age = input('Age: ')
+
+print("Hi " + name + ", you are " + str(age) + " years old.")
+
+
