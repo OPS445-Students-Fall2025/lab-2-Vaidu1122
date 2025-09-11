25544bdabe649ee3a0021aeaccff7c5d23057c1f
Vaidehi Patel <vpatel278@myseneca.ca>
2025-09-11 00:21:13 -0400

    Added Lab 2 c

---
+++ lab2c.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Name: Vaidehi Patel
+# OPS 445 NDD
+# Std ID: 166249219
+
+# Lab 2 Script 3 - Lab 2 c
+
+import sys
+
+#name = input('Name: ')
+name = sys.argv[1]
+
+#age = int(input('Age: '))
+age = int(sys.argv[2])
+
+#name = sys.argv[1]
+#age = int(sys.argv[2])
+
+print("Hi " + name + ", you are " + str(age) + " years old.")
+
+
