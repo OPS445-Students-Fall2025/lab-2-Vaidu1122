99993cbeffde44d3beefd7a8df0d76984b5273c6
Vaidehi Patel <vpatel278@myseneca.ca>
2025-09-17 18:46:48 -0400

    Added lab 2 f

---
+++ lab2f.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Author: Vaidehi Patel
+# Author ID: 166249219
+# Date Created: 2025/09/17
+# Course: OPS 445 NDD
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
+
+
